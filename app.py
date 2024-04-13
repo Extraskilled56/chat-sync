8731cf50f1fa29b9daf3c1bd04adcb81bfd8c695
Extraskilled56 <zojnes.092912@gmail.com>
2024-04-13 17:39:26 -0500

    added stuff

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    index = open("index.html", "rb")
+    return index.read()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        return open("chat.html", "rb")
+    return open("login.html", "rb")
+@app.route('/chat', methods=['get'])
+def chat():
+    return open("chat.html", "rb")
+
+if __name__ == '__main__':
+    app.run()
