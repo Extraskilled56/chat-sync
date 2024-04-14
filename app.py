e7ba98586acd3fc8fb702b27e9602da170a2bb56
Extraskilled56 <zjones.092912@gmail.com>
2024-04-13 19:31:28 -0500

    update 1.00

--- app.py
+++ app.py
@@ -7,15 +7,29 @@ app = Flask(__name__)
 def main():
     index = open("index.html", "rb")
     return index.read()
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        return open("chat.html", "rb")
-    return open("login.html", "rb")
-@app.route('/chat', methods=['get'])
+@app.route('/chat', methods=['GET', 'POST'])
 def chat():
+    sendm = open("globalchat.txt", "a")
+    if request.method == 'POST':
+        message = request.form['message']
+        username = request.form['username']
+        sendm = open("globalchat.txt", "a")
+        sendm.write("<" + username + ">:" + message + "\n") 
     return open("chat.html", "rb")
-
+@app.route('/test', methods=['get'])
+def test():
+    return open("test.html", "rb")
+@app.route('/globalchat', methods=['get'])
+def globalchat():
+    return open("globalchat.txt", "r")
+@app.route('/chat.html', methods=['GET', 'POST'])
+def login():
+    sendm = open("globalchat.txt", "a")
+    if request.method == 'POST':
+        message = request.form['message']
+        username = request.form['username']
+        sendm = open("globalchat.txt", "a")
+        sendm.write("<" + username + ">:" + message + "\n") 
+    return open("chat.html", "rb")
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
